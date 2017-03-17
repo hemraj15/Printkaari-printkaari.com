d7e0a71e6b1a9280d86d7864ca5afa16ec5dccf2
Hemraj Pamrar <hemraj.it12@gmail.com>
2017-03-18 02:56:28 +0530

    product codes added

--- printkaari_base/modules/rest-api/src/main/java/com/printkaari/rest/constant/ProductCodes.java
+++ printkaari_base/modules/rest-api/src/main/java/com/printkaari/rest/constant/ProductCodes.java
@@ -9,6 +9,8 @@ package com.printkaari.rest.constant;
  */
 public enum ProductCodes {
 	
-	hard_binding,spiral_binding,letter_head
-
+	hard_binding,spiral_binding,letter_head,college_project,prscpt_pad,bsns_card,
+	fldr_file,pamphlet,refrl_pad,envelop,broucher,opd_reg,t_shirt,bag,cup,poster,	
+	multi_clr_pmplt,leaf_let,glow_shine_board,receipt_book,i_card,folder,multi_color_file
+	
 }
